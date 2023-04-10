6c550cf532f5a2e29ba8b07954061dd6dad14515
Лана Евдокимова <lanaevdokimova@MacBook-Pro-Lana.local>
2023-04-11 02:36:50 +0300

    save tab in localStorage

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import Palletes from './components/Palletes/Palletes';
 
 function App() {
 
-  const [theme, setTheme] = useState('colours')
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'colours')
   const [filter, setFilter] = useState('')
 
+  const changeTheme = () => {
+    const newTheme = theme === 'colours' ? 'palletes' : 'colours';
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme)
+  }
+  
   return (
     <div>
       <div className='header'>
-        <button onClick={() => setTheme(theme === 'colours' ? 'palletes' : 'colours')}>Change to {theme === 'colours' ? 'palletes' : 'colours'}</button>
+        <button onClick={changeTheme}>Change to {theme === 'colours' ? 'palletes' : 'colours'}</button>
         {theme !== 'colours' && <input value={filter} onChange={(e) => setFilter(e.target.value)}/>}
         <div>
           <div>100ml = 190g ~ 50*3.6g</div>
